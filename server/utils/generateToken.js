e89c400c1cab135eec38fb67b324121e0f38c14b
zapwiz22 <kumar.jayant1916@gmail.com>
2025-06-08 14:00:52 +0530

    samesite issues

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
   });
   res.cookie("jwt", token, {
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // prevent CSRF: cross-site request forgery attacks
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // prevent CSRF: cross-site request forgery attacks
     secure: process.env.NODE_ENV === "production",
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
   });
