134c3eea3913a3e2483815dae737efb6119ace8b
zapwiz22 <kumar.jayant1916@gmail.com>
2025-06-11 23:58:03 +0530

    redis issue

--- server/db/redis.js
+++ server/db/redis.js
@@ -1,7 +1,11 @@
 import redis from "redis";
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
 export { redisClient };
+
 redisClient.on("error", (err) => {
   console.error("Redis Client Error", err);
 });
